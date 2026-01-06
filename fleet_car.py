e466880050c4d563d6bbf476ca2a6f9ada705306
Rohan Belsare <rohanbelsare113@gmail.com>
2026-01-06 18:33:22 +0530

    new questions

---
+++ fleet_car.py
@@ -0,0 +1,12 @@
+target = 10
+position = [4, 1, 0, 7]
+speed = [2, 2, 1, 1]
+
+pair = [[p, s] for p, s in zip(position, speed)]
+stack = []
+for p, s in sorted(pair)[::-1]:
+    stack.append((target - p) / s)
+    if len(stack) >= 2 and stack[-1] <= stack[-2]:
+        stack.pop()
+
+print(stack)
