66b06944015ac5dd0cd454fc46e16ce224c97a6b
Rohan Belsare <rohanbelsare113@gmail.com>
2026-02-07 10:34:44 +0530

    new code 6/2/26

---
+++ last_stone_weight.py
@@ -0,0 +1,22 @@
+import heapq
+
+stones = [1, 2]
+max_heap = []
+
+for ele in stones:
+    max_heap.append(-ele)
+
+while len(max_heap) > 1:
+    heapq.heapify(max_heap)
+    x = -heapq.heappop(max_heap)
+    y = -heapq.heappop(max_heap)
+
+    if x > y:
+        res = x - y
+        heapq.heappush(max_heap, -res)
+    elif y > x:
+        res = y - x
+        heapq.heappush(max_heap, -res)
+    else:
+        pass
+print(-max_heap[0])
