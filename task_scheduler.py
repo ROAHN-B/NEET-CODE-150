2b185a08f3d333008a33b3399becdcaafff3fe84
Rohan Belsare <rohanbelsare113@gmail.com>
2026-02-09 16:16:43 +0530

    new code 9/02/26

---
+++ task_scheduler.py
@@ -0,0 +1,22 @@
+from collections import Counter, deque
+import heapq
+
+tasks = ["X", "X", "Y", "Y"]
+n = 2
+
+count = Counter(tasks)
+max_heap = [-cnt for cnt in count.values()]
+heapq.heapify(max_heap)
+
+time = 0
+q = deque()
+
+while max_heap or q:
+    time += 1
+    if max_heap:
+        cnt = 1 + heapq.heappop(max_heap)
+        if cnt:
+            q.append([cnt, time + n])
+    if q and q[0][1] == time:
+        heapq.heappush(max_heap, q.popleft()[0])
+print(time)
